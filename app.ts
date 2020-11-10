ed20ec724e8a3165cc6a46d8517ccadf9e27f4a2
Andrew <kolbe1129@gmail.com>
2020-11-10 00:56:23 -0500

    Ready for Review

---
+++ app.ts
@@ -0,0 +1,37 @@
+const myName: string = 'Bob';
+console.log(`Name: ${myName.toUpperCase()}`);
+console.log('Career: Exotic Animal Trainer');
+console.log('Description: I like tigers');
+
+const interests = (interest: string) => {
+    console.log(`* ${interest}`);
+}
+console.log('My Interests:');
+interests('Long Walks On The Beach');
+interests('Origami');
+interests('Swinging From Rope Swings');
+interests('Bellyflopping into Public Pools');
+
+function displayPosition (jobTitle: string, companyName: string, description: string) {
+    console.log('* ' + jobTitle + ' at ' + companyName + ' - ' + description);
+};
+console.log('My Previous Experience:');
+displayPosition('Head Zebra Tamer', 'The Zoo', 'I taught all of the zebras tricks.');
+displayPosition('Air Guitar Instructor', 'Air Guitar School', 'I trained world class air guitarists.')
+displayPosition('Beekeeper', 'IHEARTBEES', 'I ate a lot of honey.')
+
+console.log('My Skills:');
+function displaySkill (skill: string, cool: boolean) {
+    if (cool == true) {
+        console.log('* ' + 'BAM: ' + skill);
+    } else {
+        console.log('* ' + skill);
+    }
+};
+displaySkill('Skateboarding', true);
+displaySkill('Extreme Cello-ing', true);
+displaySkill('Baking', false);
+displaySkill('Underwater Juggling', true);
+displaySkill('HTML', false)
+
+
